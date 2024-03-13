69873843182bb336e8723ac4f897879f1683e0f2
Mohammad Faiz <mohammadfaiz1410@gmail.com>
2024-03-14 02:28:57 +0530

    Update Flipkart.py

--- Flipkart.py
+++ Flipkart.py
@@ -6,18 +6,29 @@ service_obj = Service()
 driver = webdriver.Chrome(service=service_obj)
 
 driver.get("https://www.flipkart.com")
-driver.implicitly_wait(5)
+driver.implicitly_wait(7)
 
 #Click on mobiles
 driver.find_element(By.XPATH, "//span[text()='Mobiles']").click()
 print(driver.title)
 
+#prompt for input and  search
+product = "realme 7 Pro"
+
 #search input
-driver.find_element(By.XPATH,"//input[@class='_3704LK']").send_keys("Realme 7 pro")
+driver.find_element(By.XPATH,"//input[@class='_3704LK']").send_keys(product)
 driver.find_element(By.CSS_SELECTOR,".L0Z3Pu").click()
 
-features = driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[3]/div[1]/div[1]").text
-print(features)
+#extract deatils of product and store in variable
+#features = driver.find_element(By.XPATH,"/html/body/div/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[3]/div[1]/div[1]").text
+features = driver.find_element(By.XPATH,"//*[@id='container']/div/div[3]/div[1]/div[2]/div[2]/div/div[1]").text
 
-assert "(Mirror Blue, 128 GB)  (8 GB RAM)" in features
+#print(features)
 
+#compare details of product
+assert product in features
+
+#add to cart
+driver.find_element(By.XPATH,"//*[@id='container']/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[1]/button").click()
+#place order
+driver.find_element(By.XPATH,"//*[@id='container']/div/div[2]/div/div/div[1]/div/div[3]/div/form/button").click()
