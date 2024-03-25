c23702c79eec66598f88d320f6bee1214edafbcd
Mohammad Faiz <mohammadfaiz1410@gmail.com>
2024-03-25 13:48:35 +0530

    updated with headless

--- Flipkart_Dynamic-suggestion.py
+++ Flipkart_Dynamic-suggestion.py
@@ -1,36 +1,38 @@
 import time
-
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-service_obj = Service()
-driver = webdriver.Chrome(service=service_obj)
 
+# Create a ChromeOptions object
+chrome_options = Options()
+
+# Enable headless mode
+chrome_options.add_argument("--headless") #disable as per your need
+
+# Initialize the Chrome webdriver with the specified options
+driver = webdriver.Chrome(options=chrome_options)
+
+# Implicitly wait for elements to be found
 driver.implicitly_wait(5)
+
+# Open Flipkart website
 driver.get("https://www.flipkart.com")
 
-#Input by user
+# Input by user
 product = input("Give your Input: ")
 
-#search input
-driver.find_element(By.CSS_SELECTOR,".Pke_EE").send_keys(product)
-time.sleep(5)
+# Search input
+driver.find_element(By.CSS_SELECTOR, ".Pke_EE").send_keys(product)
+time.sleep(6)
 
-print("\nThese are the suggestion we are getting after typing",product,":")
-options = driver.find_elements(By.CSS_SELECTOR,"._3D0G9a")
-
-#total suggestions
-for option in options:
-      print(option.text.capitalize())
+# Fetch suggestions
+options = driver.find_elements(By.CSS_SELECTOR, "._3D0G9a")
 
 if len(options) >= 5:
     fifth_suggestion_text = options[4].text
-    print("\nFifth suggestion:", fifth_suggestion_text.capitalize())
+    print("\nFifth Suggestion:", fifth_suggestion_text.capitalize())
 else:
-    print("\nThere are less than 5 suggestions.")
+    print("\nThere are less than 5 Suggestions.")
 
-
-
-
-
-
+# Close the browser
+driver.quit()
