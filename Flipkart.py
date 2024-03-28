c5dcb822e27d28e3db12f2d0d5949f0e4b349de5
Mohammad Faiz <mohammadfaiz1410@gmail.com>
2024-03-28 14:13:33 +0530

    Update Flipkart.py

--- Flipkart.py
+++ Flipkart.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,56 +10,99 @@ chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://www.flipkart.com")
-driver.implicitly_wait(7)
+driver.implicitly_wait(5)
 
-#print title of phone
+# print title of phone
 Title = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!"
 if driver.title == Title:
     print("You have landed on FlipKart!!")
-
 else:
     print("Need to check site")
+    # driver.close()
 
-#prompt for input
-#product = input("Enter your product: ")
-product = "Realme 7 pro"
+#handle windows
+#windows_opened = driver.window_handles
 
-#search input
-driver.find_element(By.CSS_SELECTOR,".Pke_EE").send_keys(product)
-driver.find_element(By.CSS_SELECTOR,"._2iLD__").click()
+def selection():
+    global confirmation
+    global selected_product
+    # prompt for input
+    product = input("Enter your product: ")
+    #product = "Realme 7 pro"
 
-#click on first product
-driver.find_element(By.XPATH,"//*[@id='container']/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[2]/div[1]/div[1]").click()
-print("Clicked on 1st Product!")
+    # search input
+    driver.find_element(By.CSS_SELECTOR, ".Pke_EE").send_keys(product)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-#handle multiple windows
-windows_opened = driver.window_handles
-driver.switch_to.window(windows_opened[1])
+    # open your product
+    # product = driver.find_element(By.XPATH,"//*[@id='container']/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[2]/div[1]/div[1]").text
+    # print("Your product is :", product)
 
-#open your product
-product = driver.find_element(By.CSS_SELECTOR,".B_NuCI").text
-print("Your product is :", product)
+    #list of products
+    products = driver.find_elements(By.CSS_SELECTOR,"._4rR01T")
+    print("List of available products: 🤩")
+    # for product in products[:5]:
+    #     print(product.text)
 
-#confirmation of product
-confirmation = input("Do you really want to go with this item?\n Yes/No: ")
+    # Enumerate through the first five products
+    for index, product in enumerate(products[:5], start=1):
+        print(f"{index}. {product.text}")
+
+    # Prompt user for input to select a product
+    selected_product_index = int(input("Choose the desired option...😋: "))
+
+    # Adjust index to match list indexing
+    selected_product = products[selected_product_index - 1]
+
+    #confirmation of final product
+    print("Selected Product:", selected_product.text)
+    confirmation = input("Do you really want to go with this item?\n Yes/No: ")
+
+selection()
+
+# confirmation of product
 if confirmation.lower() == "yes":
     print("We are proceeding now...")
-else:
+
+    #click on product
+    # driver.find_element(By.XPATH,"//*[@id='container']/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a/div[2]/div[1]/div[1]").click()
+    time.sleep(5)
+
+    selected_product.click()
+    print("Clicking on final Product😍")
+
+    # handle multiple windows
+    # handle windows
+    windows_opened = driver.window_handles
+    driver.switch_to.window(windows_opened[1])
+
+
+    # click on buy button
+    wait = WebDriverWait(driver, 10)
+    wait.until(expected_conditions.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[2]/form/button")))
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[2]/form/button").click()
+
+
+    # give you number to login
+    Phone = input("Give your number to Proceed: ")
+    driver.find_element(By.XPATH, "//input[@autocomplete='off']").send_keys(Phone)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+    # enter otp
+    otp = input("Enter your OTP: ")
+    wait = WebDriverWait(driver, 10)
+    wait.until(expected_conditions.presence_of_element_located((By.XPATH, "//input[@autocomplete='on']")))
+    driver.find_element(By.XPATH, "//input[@autocomplete='on']").send_keys(otp)
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
+
+
+elif confirmation.lower() == "no":
     print("Choose your product again😥")
 
+    driver.get("https://www.flipkart.com")
+    time.sleep(3)
 
-#click on buy button
-driver.find_element(By.XPATH,"/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[2]/form/button").click()
-
-#give you number to login
-Phone = input("Give your number to Proceed: ")
-driver.find_element(By.XPATH,"//input[@autocomplete='off']").send_keys(Phone)
-driver.find_element(By.XPATH,"//button[@type='submit']").click()
-
-#enter otp
-otp = input("Enter your OTP: ")
-wait = WebDriverWait(driver, 10)
-wait.until(expected_conditions.presence_of_element_located((By.XPATH,"//input[@autocomplete='on']")))
-driver.find_element(By.XPATH,"//input[@autocomplete='on']").send_keys(otp)
-driver.find_element(By.XPATH,"//button[@type='submit']").click()
+    #click on popup bar
+    driver.find_element(By.XPATH, "/html/body/div[3]/div/span").click()
+    selection()
 
